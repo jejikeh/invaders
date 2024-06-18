d5bd491888668c9aed24a41328366d62d86c4eb5
jejikeh <jejikeh@gmail.com>
2024-06-18 18:21:33 +0300

    misc: Refactor pool. Pool now can be typed and untyped.

--- pool.go
+++ pool.go
@@ -1,28 +1,66 @@
 package gomemory
 
+import (
+	"unsafe"
+)
+
 // @Cleanup: The pool memory can be corrupted, since it embed arena, but arena allocator can allocate any type of object, and pool cannot. This can be prevented to create check in pool.Allocate for correct item size passed to method
-type Pool[T any] struct {
+
+type Pool struct {
 	*MallocArena
 
 	itemSize int
-	items    map[int]*T
+	items    map[int]unsafe.Pointer
+	indirectSize uintptr
+	alignof uintptr
 }
 
-func NewPool[T any](capacity int) *Pool[T] {
-	return &Pool[T]{
+func NewPool[T any](capacity int) *Pool {
+	return &Pool{
 		MallocArena: NewMallocArena(SizeOfAligned[T](capacity)),
 		itemSize:    SizeOfAligned[T](1),
-		items:       make(map[int]*T, capacity),
+		items:       make(map[int]unsafe.Pointer, capacity),
+		indirectSize: indirectSize(new(T)),
+		alignof: 	  unsafe.Alignof(new(T)),
 	}
 }
 
-func (p *Pool[T]) New(i int) *T {
-	t := New[T](p)
-	p.items[i] = t
-	return t
+func (p *Pool) NewAt(i int) unsafe.Pointer {
+	buf := p.Alloc(p.indirectSize, p.alignof)
+	p.items[i] = p.cursor
+	return buf
 }
 
-func (p *Pool[T]) Get(i int) (*T, bool) {
+func (p *Pool) GetAt(i int) (unsafe.Pointer, bool) {
 	item, ok := p.items[i]
 	return item, ok
+}
+
+type TypedPool[T any] struct {
+	*Pool
+}
+
+func NewTypedPool[T any](capacity int) *TypedPool[T] {
+	return &TypedPool[T]{
+		Pool: NewPool[T](capacity),
+	}
+}
+
+func ToTypedPool[T any](pool *Pool) *TypedPool[T] {
+	if SizeOfAligned[T](1) != pool.itemSize {
+		return nil
+	}
+	
+	return &TypedPool[T]{
+		Pool: pool,
+	}
+}
+
+func (p *TypedPool[T]) NewAt(i int) *T {
+	return (*T)(p.Pool.NewAt(i))
+}
+
+func (p *TypedPool[T]) GetAt(i int) (*T, bool) {
+	t, ok := p.Pool.GetAt(i)
+	return (*T)(t), ok
 }
--- pool_test.go
+++ pool_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestNewPool(t *testing.T) {
-	pool := NewPool[int](1024)
+	pool := NewTypedPool[int](1024)
 
 	if pool.itemSize != SizeOfAligned[int](1) {
 		t.Errorf("itemSize=%d but should be equal to alignedSize=%d for type [%T]", pool.itemSize, SizeOfAligned[int](1), int(0))
@@ -13,9 +13,9 @@ func TestNewPool(t *testing.T) {
 }
 
 func TestNewObjectInPool(t *testing.T) {
-	pool := NewPool[int](1024)
+	pool := NewTypedPool[int](1024)
 
-	x := pool.New(0)
+	x := pool.NewAt(0)
 	if x == nil {
 		t.Errorf("failed to allocate new object in pool")
 	}
@@ -23,17 +23,17 @@ func TestNewObjectInPool(t *testing.T) {
 
 // @Cleanup: Cleanup this test.
 func TestGetObjectFromPool(t *testing.T) {
-	pool := NewPool[int](1024)
+	pool := NewTypedPool[int](1024)
 
-	x := pool.New(0)
+	x := pool.NewAt(0)
 	if x == nil {
-		// @Incomplete: Maybe check this in .New()? If object is not allocated, it either a cast issue with memory, or malloc issue, or ovewflow.
+		// @Incomplete: Maybe check this in .NewAt()? If object is not allocated, it either a cast issue with memory, or malloc issue, or ovewflow.
 		t.Error("failed to allocate new object in pool")
 	} else {
 		*x = 123
 	}
 
-	xFromPool, _ := pool.Get(0)
+	xFromPool, _ := pool.GetAt(0)
 	if xFromPool == nil {
 		t.Error("failed to get object from pool by index")
 	} else if *xFromPool != 123 {
@@ -41,32 +41,57 @@ func TestGetObjectFromPool(t *testing.T) {
 	}
 
 	*x = 234
-	xFromPool, _ = pool.Get(0)
+	xFromPool, _ = pool.GetAt(0)
 	if xFromPool == nil {
 		t.Error("failed to get object from pool by index")
 	} else if *xFromPool != 234 {
 		t.Errorf("expected %d but got %d", 234, *xFromPool)
 	}
 
-	y := pool.New(1)
+	y := pool.NewAt(1)
 	if y == nil {
-		// @Incomplete: Maybe check this in .New()? If object is not allocated, it either a cast issue with memory, or malloc issue, or ovewflow
+		// @Incomplete: Maybe check this in .NewAt()? If object is not allocated, it either a cast issue with memory, or malloc issue, or ovewflow
 		t.Error("failed to allocate new object in pool")
 	} else {
 		*y = 102
 	}
 
-	yFromPool, _ := pool.Get(1)
+	yFromPool, _ := pool.GetAt(1)
 	if yFromPool == nil {
 		t.Error("failed to get object from pool by index")
 	} else if *yFromPool != 102 {
 		t.Errorf("expected %d but got %d", 102, *yFromPool)
 	}
 
-	xFromPool, _ = pool.Get(0)
+	xFromPool, _ = pool.GetAt(0)
 	if xFromPool == nil {
 		t.Error("failed to get object from pool by index")
 	} else if *xFromPool != 234 {
 		t.Errorf("expected %d but got %d", 234, *xFromPool)
+	}
+}
+
+func TestPool(t *testing.T) {
+	untypedPool := NewPool[int](16)
+	
+	x := (*int)(untypedPool.NewAt(1))
+	*x = 123
+	
+	ptr, _ := untypedPool.GetAt(1)
+	xFromPool := (*int)(ptr)
+	if xFromPool == nil {
+		t.Error("failed to get object from pool by index")
+	} else if *xFromPool != 123 {
+		t.Errorf("expected %d but got %d", 123, *xFromPool)
+	}
+}
+
+func TestToTypedPool(t *testing.T) {
+	untypedPool := NewPool[int](16)
+	
+	typedPool := ToTypedPool[int](untypedPool)
+	
+	if typedPool == nil {
+		t.Fail()
 	}
 }
