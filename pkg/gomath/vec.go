a8ff4d085fe928bea3f46c79c114d37f67dc48d0
jejikeh <jejikeh@gmail.com>
2024-06-22 23:00:41 +0300

    misc: MVP new Vec2 functions

--- pkg/gomath/vec.go
+++ pkg/gomath/vec.go
@@ -12,6 +12,26 @@ func NewVec2(x, y float64) *Vec2 {
 	}
 }
 
-func (v *Vec2) DivFloat(scalar float64) *Vec2 {
-	return NewVec2(v.X/scalar, v.Y/scalar)
+func NewVec2FromVec(v *Vec2) *Vec2 {
+	return &Vec2{
+		X: v.X,
+		Y: v.Y,
+	}
+}
+
+func (v *Vec2) Add(v2 *Vec2) *Vec2 {
+	v.X += v2.X
+	v.Y += v2.Y
+
+	return v
+}
+
+func (v *Vec2) Scale(scalar float64) *Vec2 {
+	if scalar == 0.0 {
+		return v
+	}
+	v.X *= scalar
+	v.Y *= scalar
+
+	return v
 }
